a9a0c9b5947b6d8c3b7521bd82ff799940e652ac
Dacha016 <dalibor.marinkovic@gmail.com>
2020-12-17 18:13:21 +0100

    promene

--- D_07_7918_Dalibor_Marinkovic/zadatak3.php
+++ D_07_7918_Dalibor_Marinkovic/zadatak3.php
@@ -1,9 +1,10 @@
 <?php
 $p = 2000;
 $m = 5000;
-$cena = 1500;
-$peraK = $p-$cena;
-$mikaK = $m-$cena;
+$k = 4000;
+$cenaDzempera = (($p+$m)-$k)/2;
+$peraK = $p - $cenaDzempera;
+$mikaK = $m - $cenaDzempera;
 echo "Kusur koji treba da dobije Pera je ".$peraK." dinara";
 echo "<br>";
 echo "Kusur koji treba da dobije Mika je ".$mikaK." dinara";
