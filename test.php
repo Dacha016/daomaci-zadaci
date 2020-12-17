4865c9f2df20afd5f8cd9008c7027ad6e651c3c5
Dacha016 <dalibor.marinkovic@gmail.com>
2020-12-17 13:57:21 +0100

    promene

---
+++ test.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    <?php
+    echo"Hello world";
+    echo"<p>Hello world! moze i ovako</p>";
+    ?>
+    </p>
+    <h1>
+    <?php echo"Zdravo svete ja sam H2 naslov"; ?></h1>
+    <p><?php 
+    echo 5 + 3;
+    echo "<br>";
+    echo 5.6-3.4;
+    echo"<br>";
+    echo "5 + 3";
+    echo"<br>";
+    echo "5" . "3";// nadovezivanje stringova (konkatenacija)
+    echo"<br>";
+    echo "5" + "3";
+    echo"<br>";
+    echo true;
+    echo"<br>";
+    echo false;
+    ?></p>
+</body>
+</html>
