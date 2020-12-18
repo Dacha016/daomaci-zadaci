20363c3d815d3b8e4f2107f48d4205cd4e0e33b5
Dacha016 <dalibor.marinkovic@gmail.com>
2020-12-18 18:38:16 +0100

    dodavanje ceil()

--- D_08_7918_Dalibor_Marinkovic/zadatak1.php
+++ D_08_7918_Dalibor_Marinkovic/zadatak1.php
@@ -4,11 +4,12 @@ $minV = 3;
 $n =40;
 $potrebnaKolicinaV = $n*$minV;
 if($potrebnaKolicinaV>$v){
-    $visakN = ($potrebnaKolicinaV-$v)/$minV;
+    $visakN =ceil(($potrebnaKolicinaV-$v)/$minV);
     echo"<p style='color:red;'>NE</p>";
     echo"<p style='color:red;'>'$visakN' osoba treba napustiti lokal!!!</p>";
 }
 else{
     echo"<p style='color:green;'>DA</p>";
 }
+
 ?>
