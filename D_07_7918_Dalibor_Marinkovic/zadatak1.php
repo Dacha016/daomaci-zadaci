d7814b0647359c8a3213d281805c3086af65a2f2
Dacha016 <dalibor.marinkovic@gmail.com>
2021-01-19 20:59:24 +0100

    obrisano

--- D_07_7918_Dalibor_Marinkovic/zadatak1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$far = 32;
-$cel = ($far-32)*5/9;
-$kel = $cel+273.15;
-echo"Temperatura u Kelvinima je ".$kel." kelvina";
-echo"<br>";
-$far = (($kel-273.15)*1.8)+32;
-echo"Temperatura u Farenhajtima je ".$far." farenhajta";
-?>
--- D_07_7918_Dalibor_Marinkovic/zadatak2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$n = 10;
-$a =2;
-$prviDan = $a;
-$drugiDan = $prviDan + 2;
-$preostalaPoglavlja = $n-($prviDan+$drugiDan);
-echo "Ostala su jos ".$preostalaPoglavlja." poglavlja";
-?>
