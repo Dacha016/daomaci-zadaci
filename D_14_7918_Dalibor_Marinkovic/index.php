6697f4af1470df47d509fcfc0430b5ef9e5d844e
Dacha016 <dalibor.marinkovic@gmail.com>
2021-01-20 00:49:22 +0100

    dorada

--- D_14_7918_Dalibor_Marinkovic/index.php
+++ D_14_7918_Dalibor_Marinkovic/index.php
@@ -34,37 +34,43 @@
             echo"<p>Trajanje filma je: $this->trajanje minuta</p>";
         }
     }
-    $f1 = new Video("Titanik",180);
-    $f2 = new Video("300",94);
-    $f3 = new Video("Mamurluk u Vegasu",105);
-    $f4 = new Video("Lov i ribolov", 45);
-    $videos = array($f1,$f2,$f3,$f4);
-    // prosecno trajanje filma
-    function prosek($videos){
-        $sum=0;
-        foreach($videos as $video){
-            $sum+=$video->getTrajanje();
-            
-        }
-        $prosecnoTraje=$sum/count($videos);
-        return $prosecnoTraje;
-    }
-    echo "Video traje prosecno: " . prosek($videos) . " minuta";
-    // film koj je najblizi prosecnom trajanju
-    function najbliziProseku($videos){
-        $sum=prosek($videos);
-        $min=abs($videos[0]->getTrajanje()-$sum);
-        $dVideo=array();
-        for($i=1;$i<count($videos);$i++){
-            $minimum=abs($videos[$i]->getTrajanje()-$sum);
-            if($min>$minimum){
-                $min=$minimum;
-                $dVideo=$videos[$i];
+        $f1 = new Video("Titanik",180);
+        $f2 = new Video("300",93);
+        $f3 = new Video("Mamurluk u Vegasu",106);
+        $f4 = new Video("Lov i ribolov", 45);
+        $f5 = new Video("Lov na vestice",106);
+        $videos = array($f1,$f2,$f3,$f4,$f5);
+        //prosek duzine videa
+        function prosek($videos){
+            $sum=0;
+            foreach($videos as $video){
+                $sum+=$video->getTrajanje();
+                
             }
-        } echo"<p><b>Video koji je najblizi proseku trajanja je: </b></p>";
-        echo $dVideo->stampaj();
-
-    }
+            $prosecnoTraje=$sum/count($videos);
+            return $prosecnoTraje;
+        }
+        echo "Video traje prosecno: " . prosek($videos) . " minuta";
+        // najblizi proseku
+        function najbliziProseku($videos){
+            $sum=prosek($videos);
+            $min=abs($videos[0]->getTrajanje()-$sum);
+            foreach($videos as $video){
+                $minimum=abs($video->getTrajanje()-$sum);
+                if($min>$minimum){
+                    $min=$minimum;
+                }
+            }
+            echo"<p><b>Video koji je najblizi proseku trajanja je: </b></p>";
+            foreach($videos as $video){
+                $minimum=abs($video->getTrajanje()-$sum);
+                if($min==$minimum){
+                    $dVideo=$video;
+                    $dVideo->stampaj();
+                    break;
+                }
+            }
+        }
     najbliziProseku($videos);
     ?>
 </body>
