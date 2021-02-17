75b9cd3fab9aa6d0fc3d6387ba7fa6943bb3094f
Dacha016 <dalibor.marinkovic@gmail.com>
2021-02-17 18:43:27 +0100

    initial commit

--- D_21_7918_Dalibor_Marinkovic/index.php
+++ D_21_7918_Dalibor_Marinkovic/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "connection.php";
+    require_once "connection.php";        
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,90 +33,108 @@ require_once "connection.php";
             </div>
             
         </form>
-        <?php
-        
-        require_once "connection.php";
-        
-        if((isset($_POST['tabela'])&& $_POST['tabela'] !='')){
-            $tabela=$conn->real_escape_string($_POST['tabela']);
-            if($tabela=='reziseri'){
-                echo '<div class="second-form"><form action="index.php"  method="POST">
-                    <label for="reziseri">Id: </label>
-                    <input type="number" name="id"><br><br>
-                    <label for="reziseri">Ime: </label>
-                    <input type="text" name="ime"><br><br>
-                    <label for="reziseri">Prezime: </label>
-                    <input type="text" name="prezime"><br><br>
-                    <label for="reziseri">Pol: </label>
-                    <input type="text" name="pol"><br><br>
-                    <input type="submit" value="Submit">
-                </form></div>';
-                if((isset($_POST['id'])&& $_POST['id'] !='') && (isset($_POST['ime']) && $_POST['ime'] !='') && (isset($_POST['prezime'])&& $_POST['prezime'] !='')){
-                    $id = $conn->real_escape_string($_POST['id']);
-                    $ime = $conn->real_escape_string($_POST['ime']);
-                    $prezime = $conn->real_escape_string($_POST['prezime']);
-                    $pol = $conn->real_escape_string($_POST['pol']);
-                    $sql="INSERT INTO reziseri (id, ime, prezime, pol) VALUES ('".$id."','".$ime."','".$prezime."', '".$pol."')";
-                    $result=$conn->query($sql);
-                    if(!$result = $conn->query($sql)){
-                        die('There was an error running the query [' . $conn->error . ']');
-                        echo "Popunite neophodna polja";
-                    }
-                    else{
-                        echo "Unos je uspesan";
-                    }
-                }
+        <?php 
+            if((isset($_POST['tabela'])&& $_POST['tabela'] !='')){
+                $tabela=$conn->real_escape_string($_POST['tabela']);
             }
-            elseif($tabela== 'zanrovi'){
-                echo '<div class="second-form"><form action="index.php"  method="POST">
-                    <label for="zanrovi">Id: </label>
-                    <input type="number" name="id"><br><br>
-                    <label for="zanrovi">Naziv: </label>
-                    <input type="text" name="naziv"><br><br>
-                    <input type="submit" value="Submit">
-                </form></div>';
-                if((isset($_POST['id'])&& $_POST['id'] !='') && (isset($_POST['naziv']) && $_POST['naziv'])){
-                    $id = $conn->real_escape_string($_POST['id']);
-                    $naziv = $conn->real_escape_string($_POST['naziv']);
-                    $sql="INSERT INTO zanrovi (id, naziv) VALUES ('".$id."','".$naziv."')";
-                    if(!$result = $conn->query($sql)){
-                        die('There was an error running the query [' . $conn->error . ']');
-                    }
-                    else{
-                        echo "Unos je uspesan";
-                    }
-                }
+            else{
+                $tabela='';
             }
-            elseif($tabela=='filmovi'){
-                echo '<div class="second-form"><form action="index.php"  method="POST">
-                    <label for="filmovi">Id: </label>
-                    <input type="number" name="id"><br><br>
-                    <label for="filmovi">Naslov: </label>
-                    <input type="text" name="naslov"><br><br>
-                    <label for="filmovi">Godina: </label>
-                    <input type="text" name="godina"><br><br>
-                    <label for="filmovi">Ocena: </label>
-                    <input type="text" name="ocena"><br><br>
-                    <label for="filmovi">Reziser id: </label>
-                    <input type="text" name="reziser_id"><br><br>
-                    <input type="submit" value="Submit">
-                </form></div>';
-                if((isset($_POST['id'])&& $_POST['id'] !='') && (isset($_POST['naziv']) && $_POST['naziv'])){
-                    $id = $conn->real_escape_string($_POST['id']);
-                    $naslov = $conn->real_escape_string($_POST['naslov']);
-                    $godina = $conn->real_escape_string($_POST['godina']);
-                    $ocena = $conn->real_escape_string($_POST['ocena']);
-                    $reziserId = $conn->real_escape_string($_POST['reziser_id']);
-                    $sql="INSERT INTO filmovi (id, naslov, godina, ocena, reziser_id) VALUES ('".$id."','".$naslov."','".$godina."', '".$ocena."', '".$reziser_id."')";
-                    if(!$result = $conn->query($sql)){
-                        die('There was an error running the query [' . $conn->error . ']');
-                    }
-                    else{
-                        echo "Unos je uspesan";
-                    }
-                }
+        if($tabela=== 'reziseri'){
+            echo '<div class="second-form"><form action="index.php"  method="POST">
+                <label for="reziseri">Id: </label>
+                <input type="number" name="id"><br><br>
+                <label for="reziseri">Ime: </label>
+                <input type="text" name="ime"><br><br>
+                <label for="reziseri">Prezime: </label>
+                <input type="text" name="prezime"><br><br>
+                <label for="reziseri">Pol: </label>
+                <input type="text" name="pol"><br><br>
+                <input type="submit" name="submit" value="Submit">
+            </form></div>';
+        }
+        if((isset($_POST['id'])&& $_POST['id'] !='') && (isset($_POST['ime']) && $_POST['ime'] !='') && (isset($_POST['prezime'])&& $_POST['prezime'] !='')){
+            $id = $conn->real_escape_string($_POST['id']);
+            $ime = $conn->real_escape_string($_POST['ime']);
+            $prezime = $conn->real_escape_string($_POST['prezime']);
+            $pol = $conn->real_escape_string($_POST['pol']);
+            $sql="INSERT INTO reziseri (id, ime, prezime, pol) VALUES ('".$id."','".$ime."','".$prezime."', '".$pol."')";
+            if(!$result= $conn->query($sql)){
+                die('There was an error running the query [' . $conn->error . ']'); 
             }
-        } 
+            else{
+                echo "Unos je uspesan";
+            }
+        }
+        if($tabela=== 'zanrovi'){
+            echo '<div class="second-form"><form action="index.php"  method="POST">
+                <label for="zanrovi">Id: </label>
+                 <input type="number" name="id"><br><br>
+                <label for="zanrovi">Naziv: </label>
+                <input type="text" name="naziv"><br><br>
+                <input type="submit" value="Submit">
+            </form></div>';
+        }
+        if((isset($_POST['id'])&& $_POST['id'] !='') && (isset($_POST['naziv']) && $_POST['naziv'])){
+            $id = $conn->real_escape_string($_POST['id']);
+            $naziv = $conn->real_escape_string($_POST['naziv']);
+            $sql="INSERT INTO zanrovi (id, naziv) VALUES ('".$id."','".$naziv."')";
+            if(!$result = $conn->query($sql)){
+                die('There was an error running the query [' . $conn->error . ']');
+            }
+            else{
+                echo "Unos je uspesan";
+            }
+        }
+        if($tabela==='filmovi'){
+            echo '<div class="second-form"><form action="index.php"  method="POST">
+                <label for="filmovi">Id: </label>
+                <input type="number" name="id"><br><br>
+                <label for="filmovi">Naslov: </label>
+                <input type="text" name="naslov"><br><br>
+                <label for="filmovi">Godina: </label>
+                <input type="text" name="godina"><br><br>
+                <label for="filmovi">Ocena: </label>
+                <input type="text" name="ocena"><br><br>
+                <label for="filmovi">Reziser id: </label>
+                <input type="text" name="reziseri_id"><br><br>
+                <input type="submit" value="Submit">
+            </form></div>';
+        }
+        if((isset($_POST['id'])&& $_POST['id'] !='') && (isset($_POST['naslov']) && $_POST['naslov'])){
+            $id = $conn->real_escape_string($_POST['id']);
+            $naslov = $conn->real_escape_string($_POST['naslov']);
+            $godina = $conn->real_escape_string($_POST['godina']);
+            $ocena = $conn->real_escape_string($_POST['ocena']);
+            $reziserId = $conn->real_escape_string($_POST['reziseri_id']);
+            $sql="INSERT INTO filmovi (id, naslov, godina, ocena, reziseri_id) VALUES ('".$id."','".$naslov."','".$godina."', '".$ocena."', '".$reziserId."')";
+            if(!$result = $conn->query($sql)){
+                die('There was an error running the query [' . $conn->error . ']');
+            }
+            else{
+                echo "Unos je uspesan";
+            }
+        }
+        if($tabela==='filmovi_zanr'){
+            echo '<div class="second-form"><form action="index.php"  method="POST">
+                <label for="filmovi_zanr">Filmo id: </label>
+                <input type="number" name="filmovi_id"><br><br>
+                <label for="filmovi_zanr">Zanr id: </label>
+                <input type="text" name="zanrovi_id"><br><br>
+                <input type="submit" value="Submit">
+            </form></div>';
+        }
+        if((isset($_POST['filmovi_id'])&& $_POST['filmovi_id'] !='') && (isset($_POST['zanrovi_id']) && $_POST['zanrovi_id'])){
+            $film_id = $conn->real_escape_string($_POST['filmovi_id']);
+            $zanr_id = $conn->real_escape_string($_POST['zanrovi_id']);
+            $sql="INSERT INTO filmovi_zanr (filmovi_id,zanrovi_id) VALUES('".$film_id."','".$zanr_id."')";
+            if(!$result = $conn->query($sql)){
+                die('There was an error running the query [' . $conn->error . ']');
+            }
+            else{
+                echo "Unos je uspesan";
+            }
+        }
         ?>
     </div>
     <div class="wrapper footer">
